ab3b9fc74768beb701181a0f85bc4791b025439c
jstrohsnitter <jstrohsnitter@gmail.com>
2024-12-04 11:57:34 -0500

    some done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,111 @@
 // src/App.jsx
 
+// import {useFighters} from 'react';
+// import {useMoney} from 'react';
+import { useState } from 'react';
+import './App.css';
+
 const App = () => {
+    const [isTeam, setIsTeam] = useState([]);
+    //const copyIsTeam = []
+    const [isMoney, setIsMoney] = useState(100);
+    const [isZombieFighters, setIsZombieFighters] = useState(
+      [
+        {
+          name: 'Survivor',
+          price: 12,
+          strength: 6,
+          agility: 4,
+          img: 'https://via.placeholder.com/150/92c952',
+        },
+        {
+          name: 'Scavenger',
+          price: 10,
+          strength: 5,
+          agility: 5,
+          img: 'https://via.placeholder.com/150/771796',
+        },
+        {
+          name: 'Shadow',
+          price: 18,
+          strength: 7,
+          agility: 8,
+          img: 'https://via.placeholder.com/150/24f355',
+        },
+        {
+          name: 'Tracker',
+          price: 14,
+          strength: 7,
+          agility: 6,
+          img: 'https://via.placeholder.com/150/d32776',
+        },
+        {
+          name: 'Sharpshooter',
+          price: 20,
+          strength: 6,
+          agility: 8,
+          img: 'https://via.placeholder.com/150/1ee8a4',
+        },
+        {
+          name: 'Medic',
+          price: 15,
+          strength: 5,
+          agility: 7,
+          img: 'https://via.placeholder.com/150/66b7d2',
+        },
+        {
+          name: 'Engineer',
+          price: 16,
+          strength: 6,
+          agility: 5,
+          img: 'https://via.placeholder.com/150/56acb2',
+        },
+        {
+          name: 'Brawler',
+          price: 11,
+          strength: 8,
+          agility: 3,
+          img: 'https://via.placeholder.com/150/8985dc',
+        },
+        {
+          name: 'Infiltrator',
+          price: 17,
+          strength: 5,
+          agility: 9,
+          img: 'https://via.placeholder.com/150/392537',
+        },
+        {
+          name: 'Leader',
+          price: 22,
+          strength: 7,
+          agility: 6,
+          img: 'https://via.placeholder.com/150/602b9e',
+        },
+      ] 
+    );
+
+const handleAddFighter = (fighterValue) => {
+    console.log(`Add Fighter`)
+    setIsTeam(isTeam.push(fighterValue))
+    console.log(isTeam)
+}
 
   return (
-    <h1>Hello world!</h1>
+    <>
+    <h1>Money: {isMoney}</h1>
+    <ul>
+      {isZombieFighters.map((fighter, index) => 
+      <li key={index}>
+        {fighter.name}<br></br>
+        {fighter.price}<br></br>
+        {fighter.strength}<br></br>
+        {fighter.agility}<br></br>
+        <img src={fighter.img}></img><br></br>
+        <button onClick={() => handleAddFighter(fighter)}>Add</button>
+      </li>
+      )}
+    </ul>
+    </>
   );
 }
 
